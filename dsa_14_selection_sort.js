1c5932cb685d2d66436899c7960f58eb2f180594
Harsh Prajapati <harsh@Harshs-MacBook-Air.local>
2024-02-21 16:44:27 +0530

    added 2 questions

---
+++ dsa_14_selection_sort.js
@@ -0,0 +1,18 @@
+// Selection Sort Algorithm
+// [0,2,34,22,10,19]
+
+function selectionSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[min]) {
+                min = j;
+            }
+        }
+    }
+    if (i !== min) {
+        temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+    }
+}
